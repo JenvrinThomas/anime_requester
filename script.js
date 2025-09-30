923ad9b44ce65df09c1b020980f0d453d1b6780a
Die tom <tom.die@etu.unicaen.fr>
2025-09-30 12:27:05 +0200

    darkmode et ajout .gitignore

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
-const data = null;
+import { API_KEY } from './config.js';
 
+const data = null;
 const xhr = new XMLHttpRequest();
 xhr.withCredentials = true;
 
@@ -10,7 +11,7 @@ xhr.addEventListener('readystatechange', function () {
 });
 
 xhr.open('GET', 'https://anime-db.p.rapidapi.com/anime?page=1&size=10&search=Fullmetal&genres=Fantasy%2CDrama&sortBy=ranking&sortOrder=asc');
-xhr.setRequestHeader('x-rapidapi-key', 'abc958223amsh3603b2a056c3ecep15863fjsn87c3f37dd4f8');
+xhr.setRequestHeader('x-rapidapi-key', API_KEY);
 xhr.setRequestHeader('x-rapidapi-host', 'anime-db.p.rapidapi.com');
 
 xhr.send(data);
