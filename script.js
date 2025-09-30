be2fb3be65f6f9d011b028d39ce1aed94ae01109
Charly ANTOINE <charly.antoine@etu-unicaen.fr>
2025-09-30 12:43:08 +0200

    ajout de la gestion du input de nom

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+const namesearch = document.getElementById('anime-name');
 const data = null;
 
 const xhr = new XMLHttpRequest();
@@ -9,8 +10,11 @@ xhr.addEventListener('readystatechange', function () {
 	}
 });
 
+
+
 xhr.open('GET', 'https://anime-db.p.rapidapi.com/anime?page=1&size=10&search=Fullmetal&genres=Fantasy%2CDrama&sortBy=ranking&sortOrder=asc');
 xhr.setRequestHeader('x-rapidapi-key', 'abc958223amsh3603b2a056c3ecep15863fjsn87c3f37dd4f8');
 xhr.setRequestHeader('x-rapidapi-host', 'anime-db.p.rapidapi.com');
 
 xhr.send(data);
+
