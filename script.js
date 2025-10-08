d0a7e89f1aa970c8266d41ceeae0d2c33bc4646d
Charly ANTOINE <charly.antoine@etu-unicaen.fr>
2025-10-08 15:39:05 +0200

    mise à jour script.js

--- script.js
+++ script.js
@@ -27,3 +27,27 @@ xhr.addEventListener('load', function () {
 });
 
 console.log(xhr);
+
+
+const API_KEY = prompt("Entrez votre clé API RapidAPI :");
+
+if (!API_KEY) {
+    alert("Vous devez fournir une clé API !");
+	API_KEY = prompt("Entrez votre clé API RapidAPI :");
+} else {
+    const data = null;
+    const xhr = new XMLHttpRequest();
+    xhr.withCredentials = true;
+
+    xhr.addEventListener('readystatechange', function () {
+        if (this.readyState === this.DONE) {
+            console.log(this.responseText);
+        }
+    });
+
+    xhr.open('GET', 'https://anime-db.p.rapidapi.com/anime?page=1&size=10&search=Fullmetal&genres=Fantasy%2CDrama&sortBy=ranking&sortOrder=asc');
+    xhr.setRequestHeader('x-rapidapi-key', API_KEY);
+    xhr.setRequestHeader('x-rapidapi-host', 'anime-db.p.rapidapi.com');
+
+    xhr.send(data);
+}
