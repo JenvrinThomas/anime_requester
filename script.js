5607a88171ce19ac8322c46ab3af9003bd49a58d
Thomas Jenvrin <thomas.jenvrin@etu.unicaen.fr>
2025-10-08 16:21:04 +0200

    correction de l'event listener du bouton

--- script.js
+++ script.js
@@ -1,45 +1,46 @@
 
 let API_KEY = prompt("Entrez votre clé API RapidAPI :");
 
+
 document.addEventListener('DOMContentLoaded', function () {
-	document.getElementById('search-button').addEventListener('click', fetchAnimeData);
-	if (!API_KEY) {
-		alert("Vous devez fournir une clé API !");
-		API_KEY = prompt("Entrez votre clé API RapidAPI :");
-	} else {
-		const data = null;
-		const xhr = new XMLHttpRequest();
-		xhr.withCredentials = true;
+	document.getElementById('search-button').addEventListener('click', () => {
+		if (!API_KEY) {
+			alert("Vous devez fournir une clé API !");
+			API_KEY = prompt("Entrez votre clé API RapidAPI :");
+		} else {
+			const data = null;
+			const xhr = new XMLHttpRequest();
+			xhr.withCredentials = true;
 
-		xhr.addEventListener('readystatechange', function () {
-			if (this.readyState === this.DONE) {
-				console.log(this.responseText);
-			}
-		});
+			xhr.addEventListener('readystatechange', function () {
+				if (this.readyState === this.DONE) {
+					console.log(this.responseText);
+				}
+			});
 
-		let titre = document.getElementById("anime-name").value;
-
-
-		xhr.open('GET', 'https://anime-db.p.rapidapi.com/anime?page=1&size=10&search=' + titre + '&sortBy=ranking&sortOrder=asc');
-		xhr.setRequestHeader('x-rapidapi-key', API_KEY);
-		xhr.setRequestHeader('x-rapidapi-host', 'anime-db.p.rapidapi.com');
-
-		xhr.responseType = 'json';
-
-		xhr.send(data);
+			let titre = document.getElementById("anime-name").value;
 
 
-		xhr.addEventListener('load', function () {
-			if (this.status === 200) {
-				console.log(this.responseText);
-			} else {
-				console.error('Erreur lors de la récupération des données');
-			}
-		});
+			xhr.open('GET', 'https://anime-db.p.rapidapi.com/anime?page=1&size=10&search=' + titre + '&sortBy=ranking&sortOrder=asc');
+			xhr.setRequestHeader('x-rapidapi-key', API_KEY);
+			xhr.setRequestHeader('x-rapidapi-host', 'anime-db.p.rapidapi.com');
 
-		console.log(xhr.responseText);
+			xhr.responseType = 'json';
 
-	}
+			xhr.send(data);
+
+
+			xhr.addEventListener('load', function () {
+				if (this.status === 200) {
+					console.log(this.responseText);
+				} else {
+					console.error('Erreur lors de la récupération des données');
+				}
+			});
+
+			console.log(xhr.responseText);
+		}
+	});
 
 
 });
